6bc1bf37ff681c24096f556bcffd7df364cd28cb
Brian Trager <brian.p.trager@outlook.com>
2021-10-28 20:16:22 -0400

    Add project files.

---
+++ Exam2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exam2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Starter Code
+            int choice = 0;
+            DisplayHeader();
+            while (true)
+            {
+                choice = GetMenuSelection();
+                switch (choice)
+                {
+                    case 1:
+                        EnterPlayerInformation();
+                        break;
+                    case 2:
+                        DisplayAllPlayers();
+                        break;
+                    case 3:
+                        FilterPlayersByYear();
+                        break;
+                    case 4:
+                        DisplayPlayerCount();
+                        break;
+                    case 5:
+                        ExitProgram();
+                        break;
+                }
+            }
+        }
+    }
+}
